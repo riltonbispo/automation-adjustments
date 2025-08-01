743d07ece13c771f32b9d9d0ed2b87656327f037
riltonbispo <riltondev@gmail.com>
2025-08-01 12:30:21 -0300

    refactor: seed employees

--- seed_employees.py
+++ seed_employees.py
@@ -1,53 +1,26 @@
+import json
+from datetime import datetime
 from db.database import SessionLocal, Base, engine
 from models.employee import RPAProposal
-from datetime import datetime, timedelta
 
 Base.metadata.create_all(bind=engine)
-
 session = SessionLocal()
 
-employee_ids = [
-    1561952,
-    1601682,
-    1601698,
-    1603834,
-    1603876,
-    1607962,
-    1607963,
-    1607971,
-    1579281,
-    1589763,
-    1563321,
-    1603853,
-    1603857,
-    1603859,
-    1601973,
-    1601276,
-    1579281,
-    1589763,
-    1563321,
-    1603853
-]
+with open("data.json", "r") as file:
+    data_para_seed = json.load(file)
 
-start_date = datetime.strptime("18/07/2025", "%d/%m/%Y").date()
-end_date = datetime.strptime("18/07/2025", "%d/%m/%Y").date()
-
-current_date = start_date
-
-while current_date <= end_date:
-    for emp_id in employee_ids:
-        new_proposal = RPAProposal(
-            date=current_date,
-            employeeId=emp_id,
-            integrationDateTime=None,
-            status=None,
-            content=None,
-            eventId=860486
-        )
-        session.add(new_proposal)
-    current_date += timedelta(days=1)
+for item in data_para_seed:
+    proposal = RPAProposal(
+        date=datetime.strptime(item["date"], "%d/%m/%Y").date(),
+        employeeId=item["employeeId"],
+        integrationDateTime=None,
+        status=None,
+        content=None,
+        eventId=860486
+    )
+    session.add(proposal)
 
 session.commit()
 session.close()
 
-print("✅ Registros inseridos com sucesso de 25/06/2025 até 25/07/2025.")
+print("✅ Registros inseridos com sucesso")
